074b3b68998b898344c7e1cfd1b2184248cfcccf
carloshrf <carlos_6282010@hotmail.com>
2020-04-17 17:36:08 -0300

    challenge completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -19,15 +19,41 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  // public all(): Transaction[] {
-  //   // TODO
-  // }
+  public all(): Transaction[] {
+    return this.transactions;
+  }
 
-  // public getBalance(): Balance {
-  //   // TODO
-  // }
+  public getBalance(): Balance {
+    const balance = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        if (transaction.type === 'income') {
+          accumulator.income += transaction.value;
+        }
+        if (transaction.type === 'outcome') {
+          accumulator.outcome += transaction.value;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
+  }
 
   public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const { total } = this.getBalance();
+
+    if (type === 'outcome' && total - value < 0) {
+      throw new Error('Not enough cash, stranger');
+    }
+
     const transaction = new Transaction({ title, value, type });
 
     this.transactions.push(transaction);
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 
-import Transaction from '../models/Transaction';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 
@@ -8,13 +7,16 @@ const transactionRouter = Router();
 
 const transactionsRepository = new TransactionsRepository();
 
-// transactionRouter.get('/', (request, response) => {
-//   try {
-//     // TODO
-//   } catch (err) {
-//     return response.status(400).json({ error: err.message });
-//   }
-// });
+transactionRouter.get('/', (request, response) => {
+  try {
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({ transactions, balance });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
 
 transactionRouter.post('/', (request, response) => {
   try {
